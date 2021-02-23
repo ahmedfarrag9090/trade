c2ff0d579fa53b7678a1c5597a035747d6808e5c
ahmedfarrag9090 <farrag9090@gmail.com>
2021-02-23 13:22:34 +0200

    some edit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
-$(function(){
+$(function () {
 
-    $('.log a').on('click', function(){
-
-        if($(this).siblings('.log-form').css('display') == "none"){
-            $(this).siblings('.log-form').fadeIn();
+    //  show login form
+    $('.log a').on('click', function (e) {
+        e.preventDefault();
+        if ($('.log-form').css('display') === "none") {
+            $('.log-form').fadeIn();
             $(this).css('background-color', '#eee')
-        }else if($(this).siblings('.log-form').css('display') == "block"){
-            $(this).siblings('.log-form').fadeOut();
+        } else {
+            $('.log-form').fadeOut();
             $(this).css('background-color', '');
         }
-
     });
 
-    $('html, body').on('click', function(e){
-        if($(e.target).closest('.log a, .log-form').length)
+    //  hide login form when user click body
+    $('html, body').on('click', function (e) {
+        if ($(e.target).closest('.log a, .log-form').length)
             return;
-        if($('.log a').siblings('.log-form').css('display') === "block"){
-            $('.log a').siblings('.log-form').fadeOut();
-            $('.log a').css('background-color', '');
-        }
+        $('.log-form').fadeOut();
+        $('.log a').css('background-color', '');
     });
 
-    var srchBtn = $('.search');
 
-    srchBtn.on('click', function(){
+    //  show search bar
+    var srchBtn = $(".search");
 
-            $(this).prev('input').show()
+    srchBtn.on('click', function () {
+
+        $(this).prev('input').show()
             .parents('form').css({
                 'width': '100%',
                 'height': '100%'
@@ -38,28 +39,30 @@ $(function(){
 
     });
 
-    $('html, body').click(function(e){    
-            if($(e.target).closest('input, i').length)
-                return;
+    //  close search bar
+    $('html, body').click(function (e) {
+        if ($(e.target).closest('input, i').length)
+            return;
 
-                srchBtn.fadeIn().next('i').hide().end()
-                .prev('input').hide()
-                .parents('form').css({'width': 'auto', 'height': 'auto'})
-                .siblings().not('#navlinks').fadeIn()
-                .siblings('#navlinks').css({
-                    'visibility': 'visible',
-                    'width': 'auto'
-                });
- 
+        srchBtn.fadeIn().next('i').hide().end()
+            .prev('input').hide()
+            .parents('form').css({ 'width': 'auto', 'height': 'auto' })
+            .siblings().not('#navlinks').fadeIn()
+            .siblings('#navlinks').css({
+                'visibility': 'visible',
+                'width': 'auto'
+            });
+
     });
 
-    $(window).on('scroll', function(){
+    //  reduce nav size when user scroll down
+    $(window).on('scroll', function () {
 
-        if($(this).scrollTop() > 45 && $(this).outerWidth() >= 992){
+        if ($(this).scrollTop() > 45) {
             $('header').addClass('fixed-top');
             $('.navbar-brand').css('font-size', '1.3rem');
             $('.nav-item > a').removeClass('py-lg-5');
-        }else{
+        } else {
             $('header').removeClass('fixed-top');
             $('.navbar-brand').css('font-size', '2.3rem');
             $('.nav-item > a').addClass('py-lg-5');
@@ -67,28 +70,29 @@ $(function(){
     });
 
 
-    $('.dropdown').on({
-        'mouseenter' : function(){
-            if($(this).children('.dropdown-menu').css('display') == 'none'){
-                $(this).children('.dropdown-menu').delay(300).fadeIn();
+    //   show drop menu below nav links
+    $(".dropdown-toggle").on('click', function (e) {
+        e.preventDefault()
+        $(this).parents('.dropdown').siblings('.dropdown').children(".dropdown-menu").fadeOut();
 
-            }
-            
-        },
-
-        'mouseleave' : function(){
-
-            // if($(this).children('.dropdown-menu').css('display') == 'block'){
-                $(this).children(".dropdown-menu").delay(300).fadeOut();
-
-            // }
-            
+        if ($(this).siblings('.dropdown-menu').css('display') === "none") {
+            $(this).siblings('.dropdown-menu').fadeIn();
+        } else {
+            $(this).siblings(".dropdown-menu").fadeOut();
 
         }
     });
 
+    // hide drop menu when user click body
+    $('html, body').on('click', function (e) {
+        if ($(e.target).closest('.dropdown-toggle, .dropdown-menu').length === 1)
+            return;
+        $('.dropdown-menu').fadeOut();
+    });
 
-    $('.title a').click(function(){
+
+    //   toggle products  rows
+    $('.products-area .title a').click(function () {
         $(this).addClass('active').parents('div').siblings().children('a').removeClass('active')
         $($(this).data('target')).fadeIn(300).siblings().fadeOut(300)
     });
